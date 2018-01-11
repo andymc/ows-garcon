ceda512e8fdfa3f7ce83902cfb80a9a47e2b2328
John Penner <jpenner@gmail.com>
2018-01-11 10:53:09 -0500

    Revert "Add logger to DeciderWorker"
    This reverts commit 68b0a5211aaeccb77c7bc971b5246427a5a8592b.

--- garcon/decider.py
+++ garcon/decider.py
@@ -12,13 +12,13 @@ from boto.swf.exceptions import SWFTypeAlreadyExistsError
 import boto.swf.layer2 as swf
 import functools
 import json
+import traceback
 
 from garcon import activity
 from garcon import event
-from garcon import log
 
 
-class DeciderWorker(swf.Decider, log.GarconLogger):
+class DeciderWorker(swf.Decider):
 
     def __init__(self, flow, register=True):
         """Initialize the Decider Worker.
@@ -138,7 +138,6 @@ class DeciderWorker(swf.Decider, log.GarconLogger):
             decisions.fail_workflow_execution(reason=str(e))
             if self.on_exception:
                 self.on_exception(self, e)
-            self.logger.error(e, exc_info=True)
 
     def delegate_decisions(self, decisions, decider, history, context):
         """Delegate the decisions.
@@ -179,7 +178,6 @@ class DeciderWorker(swf.Decider, log.GarconLogger):
             decisions.fail_workflow_execution(reason=str(e))
             if self.on_exception:
                 self.on_exception(self, e)
-            self.logger.error(e, exc_info=True)
 
     def run(self, identity=None):
         """Run the decider.
@@ -216,7 +214,7 @@ class DeciderWorker(swf.Decider, log.GarconLogger):
             # when such an exception occurs.
             if self.on_exception:
                 self.on_exception(self, error)
-            self.logger.error(error, exc_info=True)
+            # todo: Add log exception
             return True
 
         custom_decider = getattr(self.flow, 'decider', None)
